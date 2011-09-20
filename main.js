a97cc75b733f1fa0320303dafb62f5befd2d27fd
PuercoPop <pirata@gmail.com>
2011-09-20 00:44:41 -0500

    mockupfiles

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function on_submit()
+{
+  /*Se llama a la función de python*/
+}
+
+function post_submit()
+{
+ /*Una vez que ya se subio la foto y se grabaron los potenciales retratos se les tiene que incorpoar en la página*/ 
+}
