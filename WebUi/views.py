63bdbe708727513c001d6b64a730aaf9c03022a6
PuercoPop <pirata@gmail.com>
2011-09-26 02:08:57 -0500

    Revert "Restored Previous html"
    This reverts commit 8c1d14af0b2e30b048b2f6c4165947a4f8bbee7a.
    
    	modified:   Templates/upload_photo.html
    	modified:   WebUi/views.py

--- WebUi/views.py
+++ WebUi/views.py
@@ -4,28 +4,41 @@ import testhaar
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
+import forms as webuiforms
 
 def MainPage(request):
   return render_to_response( 'main_page.html', {} )
 
 @csrf_exempt
 def UploadPhoto(request):
+  #c = {}
+  #c.update(csrf(request))
   if request.method == 'POST':
-    print request.FILES
-    photo_path = request.FILES['name']#    handle_uploaded_file(request.FILES['file'], request.FILES['name'])    
-  
+#    print request.FILES
+#    photo_path = request.FILES['name']
+#    handle_uploaded_file(request.FILES['file'], request.FILES['name'])    
     #portraits = find_face(,)
     
-    return render_to_response( 'upload_photo.html', { 'photo_path':'' ,'portraits':'',})#portraits } )
+    form = webuiforms.PhotoForm(request.POST, request.FILES)
+    if form.is_valid():
+      handle_uploaded_file(request.FILES['photo'])
+      return render_to_response( 'upload_photo.html', { 'photo_path':'' ,'portraits':'', 'ispost': True, 'uploaded_photo_ok' : True })#,c)#portraits } )
   else:
       form = webuiforms.PhotoForm()
-  return render_to_response( 'main_page.html', { 'ispost': false, 'uploaded_photo_ok' : false} )    
-  
+#  return render_to_response( 'main_page.html', { 'ispost': false, 'uploaded_photo_ok' : false} )    
+  return render_to_response( 'upload_photo.html', { 'photo_path':'' ,'portraits':'', 'ispost': request.method == 'POST' , 'uploaded_photo_ok' : False, 'form': form })
 
-
+"""
 def handle_uploaded_file(file_data,file_name):
     destination = open( file_name, 'wb+')
     for chunk in data.chunks():
+        destination.write(chunk)
+    destination.close()
+"""
+
+def handle_uploaded_file(f):
+    destination = open('TEST_CASE1.png', 'wb+')
+    for chunk in f.chunks():
         destination.write(chunk)
     destination.close()
     
