e4efda5d4f21bfd1d7ba583e218204f3a8b27774
PuercoPop <pirata@gmail.com>
2011-10-07 02:20:45 -0500

    fixed cv.LoadImage path

--- WebUi/testhaar.py
+++ WebUi/testhaar.py
@@ -2,31 +2,6 @@ import cv
 import os
 from flag import __server__
 
-
-def find_faces_dir( directory ):
-  cascade = cv.Load('./haarcascade_frontalface_alt.xml')
-
-  for counter_imagen , image in enumerate(os.listdir(directory)):
-    img = cv.LoadImage(directory+image)
-    imgGray = cv.CreateImage( cv.GetSize(img), img.depth , 1) 
-    cv.CvtColor(img, imgGray, cv.CV_BGR2GRAY)
-  
-    faces = cv.HaarDetectObjects( imgGray, cascade , cv.CreateMemStorage(),)
-
-    if len(faces)>0:
-      print "Detecto Algo"
-    else:
-      print "Miss"
-
-    for counter , ((x, y, w, h), n) in enumerate(faces):
-      cv.SetImageROI(img, (x,y,w,h ) )#Fija la region de interes
-      imgface = cv.CreateImage( cv.GetSize(img),img.depth,img.nChannels)
-      imgface_rsz = cv.CreateImage( (128,128) ,img.depth,img.nChannels)
-      cv.Copy(img,imgface)
-      cv.Resize(imgface, imgface_rsz, cv.CV_INTER_AREA)
-      cv.SaveImage("Temp_Faces/face_" +str(image[:-4])+"_"+str(counter)+".png",imgface_rsz)
-      cv.ResetImageROI(img)
-
 def find_faces(  img_url ):
   if __server__ == "home":
     cascade = cv.Load('/home/pirata/Data/Pucp/2011-2/Tesis 2/FaceRec/WebUi/haarcascade_frontalface_alt.xml')
@@ -34,7 +9,7 @@ def find_faces(  img_url ):
     target_directory = 'Uploads/Portraits/'
   elif __server__ == "production":
     cascade = cv.Load('/home/puercopop/webapps/django/myproject/WebUi/haarcascade_frontalface_alt.xml')
-    directory = 'Uploads/'
+    directory = '/home/puercopop/webapps/django/myproject/Uploads/'
     target_directory = '/home/puercopop/webapps/django/myproject/Uploads/Portraits/'
     
   portrait_list = []
