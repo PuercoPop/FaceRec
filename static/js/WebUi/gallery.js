5dbe1c00b44c120406649c98f19d969f3e817f78
PuercoPop <pirata@gmail.com>
2012-02-10 19:20:49 -0500

    Almost done with the logging of User Classifcation And License

--- static/js/WebUi/gallery.js
+++ static/js/WebUi/gallery.js
@@ -3,12 +3,23 @@ $(document).ready(function(){
     $('input.accept-face').click(function(){	
 	name_face($('input[type=text][portrait-id=' + $(this).attr('portrait-id') + ']').val(),
 		  $(this).attr('portrait-id'));
-	/* css magic goes here*/
+	/* css magic goes here */
+	$('ul[portrait-id=' + $(this).attr('portrait-id') + '] > li.portrait-name').html(
+	    '<label>' + $('input[type=text][portrait-id=' + $(this).attr('portrait-id') + ']').val() + '</label>'
+	);
+	$('ul[portrait-id=' + $(this).attr('portrait-id') + '] > li.portrait-input').html(
+	    '<input portrait-id=" ' + $(this).attr('portrait-id') +' " class="reject-face" type="button" value="No es un Rostro">'
+	);
     });
 
     $('input.reject-face').click(function(){
 	not_face($(this).attr('portrait-id'));
-	
+	$('ul[portrait-id=' + $(this).attr('portrait-id') + '] > li.portrait-name').html(
+	    '<input portrait-id="' + $(this).attr('portrait-id') +'" type="text">'
+	);
+	$('ul[portrait-id=' + $(this).attr('portrait-id') + '] > li.portrait-input').html(
+	    '<input portrait-id=" ' + $(this).attr('portrait-id') +' " class="accept-face" type="button" value="Nombrar Rostro">'
+	);
     });
 
 });
--- static/js/WebUi/upload_photo.js
+++ static/js/WebUi/upload_photo.js
@@ -6,9 +6,24 @@ $(document).ready(function() {
     $('input.accept-face').click(function(){
 	name_face($('input[type="text"][portrait-id="'+ $(this).attr('portrait-id')+ '"]').val(), $(this).attr('portrait-id')  )
 	/*other logic here */
+	$('ul[portrait-id=' + $(this).attr('portrait-id') + '] > li.portrait-name').html(
+	    '<label>' + $('input[type=text][portrait-id=' + $(this).attr('portrait-id') + ']').val() + '</label>'
+	);
+	$('ul[portrait-id=' + $(this).attr('portrait-id') + '] > li.portrait-input').html(
+	    '<input portrait-id="' + $(this).attr('portrait-id') + '" class="reject-face" type="button" value="No es un Rostro">'
+	);
+	$('img[portrait-id="' + $(this).attr('portrait-id') + '"]').removeClass('not-face');
+
     });
     $('input.reject-face').click(function(){
 	not_face($(this).attr('portrait-id'));
+	$('ul[portrait-id=' + $(this).attr('portrait-id') + '] > li.portrait-name').html(
+	    '<input portrait-id="' + $(this).attr('portrait-id') +'" type="text">'
+	);
+	$('ul[portrait-id=' + $(this).attr('portrait-id') + '] > li.portrait-input').html(
+	    '<input portrait-id=" ' + $(this).attr('portrait-id') +' " class="accept-face" type="button" value="Nombrar Rostro">'
+	);
+	$('img[portrait-id="' + $(this).attr('portrait-id') +'"]').addClass('not-face')
     });
   
 });
